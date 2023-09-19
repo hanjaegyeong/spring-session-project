befe1f2d182f23a790798a2378dc3baae8d3f922
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-09-19 16:33:14 +0900

    feat: signup 폼 받는 POST 컨트롤러 추가

--- src/main/java/com/example/springsessionproject/controller/LoginController.java
+++ src/main/java/com/example/springsessionproject/controller/LoginController.java
@@ -1,8 +1,16 @@
 package com.example.springsessionproject.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+
+@Slf4j
 @Controller
 public class LoginController {
 
@@ -11,9 +19,17 @@ public class LoginController {
         return "signup";
     }
 
+    //username, password
+    @PostMapping("/signup")
+    public void save(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        log.info("username={}, password={}", username, password);
+        response.getWriter().write("ok");
+    }
+
     @GetMapping("/signin")
     public String signin() {
         return "signin";
     }
-
 }
