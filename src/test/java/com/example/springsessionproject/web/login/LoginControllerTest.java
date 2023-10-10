a394307c399b09de549adc5e13fbc3b2a8c5064f
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-10 17:14:47 +0900

    feat: 로그인 테스트에 회원가입 코드 추가

--- src/test/java/com/example/springsessionproject/web/login/LoginControllerTest.java
+++ src/test/java/com/example/springsessionproject/web/login/LoginControllerTest.java
@@ -3,6 +3,7 @@ package com.example.springsessionproject.web.login;
 import com.example.springsessionproject.SessionConst;
 import com.example.springsessionproject.domain.login.LoginService;
 import com.example.springsessionproject.domain.member.Member;
+import com.example.springsessionproject.domain.member.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
@@ -10,7 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -31,9 +34,14 @@ public class LoginControllerTest {
     @Mock
     private LoginService loginService;
 
+    @Autowired
     private LoginController loginController;
 
     private MockHttpSession session;
+
+    @MockBean
+    private MemberRepository memberRepository; // @MockBean을 사용하여 Mock MemberRepository 주입
+
 
     @BeforeEach
     public void setUp() {
@@ -44,46 +52,21 @@ public class LoginControllerTest {
 
     @Test
     public void testSessionSearchTime() {
-        Member fakeMember = new Member("fakeUserId", "fakePassword");
+        String userId = "fakeUserId";
+        String password = "fakePassword";
+
+        Member fakeMember = new Member(userId, password);
 
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setSession(session);
 
         // Create a LoginForm object
-        LoginForm loginForm = new LoginForm("fakeUserId", "fakePassword");
+        LoginForm loginForm = new LoginForm(userId, password);
 
         // Create a BindingResult object
         BindingResult bindingResult = new BeanPropertyBindingResult(loginForm, "loginForm");
 
-        when(loginService.login("fakeUserId", "fakePassword")).thenReturn(fakeMember);
-
-        long startTime = System.currentTimeMillis();
-        String viewName = loginController.login(loginForm, bindingResult, request);
-        long endTime = System.currentTimeMillis();
-
-        assertNotNull(session.getAttribute(SessionConst.LOGIN_MEMBER));
-        assertEquals("redirect:/home", viewName);
-
-        long searchTime = endTime - startTime;
-        log.info("Session search time: " + searchTime + " milliseconds");
-    }
-
-
-    @RepeatedTest(50)
-    @Test
-    public void iterTestSessionSearchTime() {
-        Member fakeMember = new Member("fakeUserId", "fakePassword");
-
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setSession(session);
-
-        // Create a LoginForm object
-        LoginForm loginForm = new LoginForm("fakeUserId", "fakePassword");
-
-        // Create a BindingResult object
-        BindingResult bindingResult = new BeanPropertyBindingResult(loginForm, "loginForm");
-
-        when(loginService.login("fakeUserId", "fakePassword")).thenReturn(fakeMember);
+        when(loginService.login(userId, password)).thenReturn(fakeMember);
 
         long startTime = System.currentTimeMillis();
         String viewName = loginController.login(loginForm, bindingResult, request);
@@ -98,22 +81,27 @@ public class LoginControllerTest {
 
     @Test
     public void iterAvgTestSessionSearchTime() {
-        int numberOfTests = 200; // 테스트를 20번 반복 실행
+        int numberOfTests = 1000; // 테스트 반복실행 횟수
         long totalSearchTime = 0; // 검색 시간 누적을 위한 변수
+        String userId = "fakeUserId";
+        String password = "fakePassword";
+
+        // 회원 가입
+        Member fakeMember = new Member(userId, password);
+        memberRepository.save(fakeMember);
 
         for (int i = 0; i < numberOfTests; i++) {
-            Member fakeMember = new Member("fakeUserId", "fakePassword");
 
             MockHttpServletRequest request = new MockHttpServletRequest();
             request.setSession(session);
 
-            // Create a LoginForm object
-            LoginForm loginForm = new LoginForm("fakeUserId", "fakePassword");
+            // LoginForm 객체 생성
+            LoginForm loginForm = new LoginForm(userId, password);
 
-            // Create a BindingResult object
+            // BindingResult 객체 생성
             BindingResult bindingResult = new BeanPropertyBindingResult(loginForm, "loginForm");
 
-            when(loginService.login("fakeUserId", "fakePassword")).thenReturn(fakeMember);
+            when(loginService.login(userId, password)).thenReturn(fakeMember);
 
             long startTime = System.currentTimeMillis();
             String viewName = loginController.login(loginForm, bindingResult, request);
@@ -125,6 +113,10 @@ public class LoginControllerTest {
             long searchTime = endTime - startTime;
             totalSearchTime += searchTime;
             log.info("Session search time: " + searchTime + " milliseconds");
+
+            // 세션 클리어
+            session.clearAttributes();
+
         }
         // 평균 검색 시간 계산 및 출력
         long averageSearchTime = totalSearchTime / numberOfTests;
