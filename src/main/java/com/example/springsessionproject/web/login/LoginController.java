6cff1fed7ffef6e86a587082f79366ff369ce71e
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-10 09:56:47 +0900

    feat: 로그인 기능 추가-세션 적용

--- src/main/java/com/example/springsessionproject/web/login/LoginController.java
+++ src/main/java/com/example/springsessionproject/web/login/LoginController.java
@@ -1,10 +1,18 @@
 package com.example.springsessionproject.web.login;
 
+import com.example.springsessionproject.SessionConst;
+import com.example.springsessionproject.domain.login.LoginService;
+import com.example.springsessionproject.domain.member.Member;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,13 +20,33 @@ import java.io.IOException;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final LoginService loginService;
+
     @GetMapping("/signin")
-    public String signin() {
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "signin";
     }
 
+    @PostMapping("/signin")
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "signin";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
 
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다"); //reject: global error(검증 단원에서 배움)
+            return "signin";
+        }
 
+        //로그인 성공 처리
+        //세션이 있으면 해당 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        return "redirect:/home";
+    }
 }
